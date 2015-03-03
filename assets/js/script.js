7f03bb00e846e3f07d0a663765a5b285259b6308
KITE <ixkaito@gmail.com>
2015-03-03 20:52:37 +0900

    Add side navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+$(function() {
 
+  (function sideNav() {
+    var $main = $('.js-main');
+    var $h2 = $main.find('h2');
+    var $nav = $('.js-site-nav');
+    var $navs = $nav.children('ul');
+    var index, text;
+
+    $h2.each(function() {
+      index = $h2.index(this);
+      text = $(this).text();
+      $(this).attr('id', 'h2-' + index );
+      $navs.append('<li><a href="#h2-' + index + '">' + text + '</a></li>');
+    });
+  })();
+
+});
