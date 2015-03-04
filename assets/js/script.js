11e402b52c43301b82b7bf2e96c365fa2cb7ea5d
KITE <ixkaito@gmail.com>
2015-03-04 09:41:05 +0900

    Enable affix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 $(function() {
 
   (function sideNav() {
-    var $main = $('.js-site-main');
-    var $h2 = $main.find('h2');
-    var $nav = $('.js-site-nav');
-    var $navs = $nav.children('ul');
+    var $content  = $('.js-site-content');
+    var contentH  = $content.height();
+    var $main     = $('.js-site-main');
+    var $h2       = $main.find('h2');
+    var $footer   = $('.js-site-footer');
+    var footerTop = $footer.offset().top;
+    var $aside    = $('.js-site-aside');
+    var $nav      = $('.js-site-nav');
+    var $navs     = $nav.children('ul');
+    var $affix    = $('.js-affix');
+    var affixW    = $affix.width();
+    var affixH    = $affix.height();
+    var affixTop  = $affix.offset().top;
+    var scrlTop   = $(window).scrollTop();
     var index, text;
 
     $h2.each(function() {
@@ -18,6 +28,28 @@ $(function() {
       $navs.children('li').removeClass('active');
       $(this).addClass('active');
     });
+
+    $aside.css({
+      height: contentH + 'px'
+    });
+
+    $affix.css({
+      width: affixW + 'px'
+    });
+
+    $(window).on('scroll', function() {
+      scrlTop = $(window).scrollTop();
+      affixH  = $affix.height();
+      console.log( 'scrlTop: ' + scrlTop + ', affixTop: ' + affixTop + ', footerTop: ' + footerTop + ', affixH: ' + affixH);
+      if ( scrlTop > affixTop && scrlTop <= (footerTop - affixH) ) {
+        $affix.removeClass('affix-bottom').addClass('affix');
+      } else if ( scrlTop > affixTop && scrlTop > (footerTop - affixH) ) {
+        $affix.removeClass('affix').addClass('affix-bottom');
+      } else {
+        $affix.removeClass('affix');
+      }
+    });
+
   })();
 
 });
