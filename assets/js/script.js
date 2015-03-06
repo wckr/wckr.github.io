420a9c32c62d748999a0d068ecb0658b9abb890f
KITE <ixkaito@gmail.com>
2015-03-06 18:47:09 +0900

    Show issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,12 @@ $(function() {
     var affixTop  = $affix.offset().top;
     var scrlTop   = $(window).scrollTop();
     var navsAbove = 1;
-    var index, text, h2Top, h2NextTop, $navsItem;
+    var text, h2Top, h2NextTop, $navsItem;
 
-    $h2.each(function() {
-      index = $h2.index(this);
+    $h2.each(function(i) {
       text = $(this).text();
-      $(this).attr('id', 'h2-' + index );
-      $navs.append('<li><a href="#h2-' + index + '">' + text + '</a></li>');
+      $(this).attr('id', 'h2-' + i );
+      $navs.append('<li><a href="#h2-' + i + '">' + text + '</a></li>');
     });
 
     $navs.on('click', 'li', function() {
@@ -49,15 +48,14 @@ $(function() {
         $affix.removeClass('affix');
       }
 
-      $h2.each(function() {
-        index = $h2.index(this);
+      $h2.each(function(i) {
         $navsItem = $navs.find('li');
         h2Top = Math.floor($(this).offset().top);
         h2FirstTop = Math.floor($h2.eq(0).offset().top);
-        if (index === $h2.length - 1) {
+        if (i === $h2.length - 1) {
           h2NextTop = $('body').height();
         } else {
-          h2NextTop = Math.floor($h2.eq(index + 1).offset().top);
+          h2NextTop = Math.floor($h2.eq(i + 1).offset().top);
         }
 
         if ( scrlTop === 0 ) {
@@ -65,7 +63,7 @@ $(function() {
           $navsItem.eq(0).addClass('active');
         } else if ( scrlTop >= (h2Top - $(window).height() / 2) && scrlTop < (h2NextTop - $(window).height() / 2) ) {
           $navsItem.removeClass('active');
-          $navsItem.eq(index + navsAbove).addClass('active');
+          $navsItem.eq(i + navsAbove).addClass('active');
         }
       });
     });
@@ -73,3 +71,16 @@ $(function() {
   })();
 
 });
+
+function show_issues(res) {
+  var issues  = res.data;
+  var $issues = $('.js-site-issues');
+  console.log(issues);
+  if (issues.length) {
+    var $ul = $('<ul />');
+    $(issues).each(function(){
+      $ul.append('<li><a href="' + this.html_url + '" title="' + this.login + '">' + this.title + '</a></li>');
+    });
+    $issues.append($ul);
+  }
+}
